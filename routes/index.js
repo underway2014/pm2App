ceaf96d4c2c59cf4f1d52d2e3404eb776dce7922
binli.home.deepin <646644017@qq.com>
2017-08-20 18:12:10 +0800

    cc

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
 //   res.render('index', { title: 'Express' });
-res.send('deploy test, B');
+res.send('deploy test, C');
 });
 
 module.exports = router;
